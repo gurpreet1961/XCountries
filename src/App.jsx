9e04c7e1b6f56bc3d7d86a437df2ee7398a7b312
Gurpreet Singh <gurpreet012guru@gmail.com>
2024-01-27 11:47:44 +0530

    ME_FE_BUILDOUT_XCOUNTRIES_MODULE_ONE

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+const App = () => {
+	const [Countries, setCountries] = useState([]);
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const url = "https://restcountries.com/v3.1/all";
+				const res = await axios(url, {
+					method: "GET",
+				});
+				const data = await res.data;
+
+				setCountries(data);
+			} catch (err) {
+				console.error(err);
+			}
+		};
+		fetchData();
+	});
+
+	return (
+		<div className="container">
+			{Countries?.map((ele) => {
+				return (
+					<div key={ele.cca3} className="card">
+						<img src={ele.flags.png} alt={ele.flags.alt} />
+						<h4>{ele.name.common}</h4>
+					</div>
+				);
+			})}
+		</div>
+	);
+};
+
+export default App;
